a1a4688ffef59c1836a69fe58e403f57f322ec25
StallPlayz <stallplayz123@gmail.com>
2025-09-18 17:32:38 +0700

    Update | Finishing 11th lesson

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
-const angka1 = [1, 2, 3];
-
-console.log(...angka1);
-// Output: 1 2 3
-
-const buah1 = ["Apel", "Jeruk"];
-const buah2 = ["Mangga", "Pisang"];
-
-const semuaBuah = [...buah1, ...buah2];
-console.log(semuaBuah);
-// Output: ["Apel", "Jeruk", "Mangga", "Pisang"]
-
-const warnaAsli = ["Merah", "Biru"];
-const warnaSalinan = [...warnaAsli];
-
-warnaSalinan.push("Hijau");
-
-console.log(warnaAsli);   // ["Merah", "Biru"]
-console.log(warnaSalinan); // ["Merah", "Biru", "Hijau"]
-
-const user = {
-  nama: "Dina",
-  umur: 25
-};
-
-const userBaru = {
-  ...user,
-  pekerjaan: "Programmer"
-};
-
-console.log(userBaru);
-// Output: { nama: 'Dina', umur: 25, pekerjaan: 'Programmer' }
-
-function tambah(a, b, c) {
-  return a + b + c;
+function tambah(a, b) {
+  return a + b;
 }
 
-const angka = [1, 2, 3];
+//Bagaimana jika kita harus menerima banyak argumen? berikut :
 
-console.log(tambah(...angka));
-// Output: 6
+function jumlahkan(...angka) {
+  let total = 0;
+  for (let nilai of angka) {
+    total += nilai;
+  }
+  return total;
+}
+
+console.log(jumlahkan(1, 2));        // Output: 3
+console.log(jumlahkan(1, 2, 3, 4));  // Output: 10
+
+function cetakData(nama, ...hobi) {
+  console.log("Nama:", nama);
+  console.log("Hobi:", hobi);
+}
+
+cetakData("Budi", "Mancing", "Ngoding", "Main bola");
+
+/* Output: 
+
+Nama: Budi
+
+Hobi: ["Mancing", "Ngoding", "Main bola"] */
+
+const angka = [1, 2, 3, 4, 5];
+
+const [pertama, kedua, ...sisanya] = angka;
+
+console.log(pertama); // 1
+console.log(kedua);   // 2
+console.log(sisanya); // [3, 4, 5]
+
+const user = {
+  nama: "Siti",
+  umur: 20,
+  kota: "Bandung"
+};
+
+const { nama, ...infoLain } = user;
+
+console.log(nama);      // Siti
+console.log(infoLain);  // { umur: 20, kota: 'Bandung' }
+
+
 
