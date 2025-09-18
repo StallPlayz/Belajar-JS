c67414097cd9eb2850b6ad9e3d0eb122a8c7d41c
StallPlayz <stallplayz123@gmail.com>
2025-09-18 14:55:37 +0700

    Update | Finishing 8th lesson

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
-const sayHello = () => {
-  console.log("Halo, Dunia!");
-};
+let buah = ["Apel", "Pisang", "Jeruk"];
 
-const greet = (nama) => {
-  console.log("Halo, " + nama + "!");
-};
+console.log(buah);
 
-greet("Budi"); // Output: Halo, Budi!
+let angka = new Array(1, 2, 3, 4, 5);
 
-const helloWorld = () => {
-  console.log("Hello, World!");
-};
+console.log(angka);
 
-helloWorld();
+let hewan1 = ["Kucing", "Anjing", "Kelinci"];
 
-const sapa = nama => {
-  console.log("Hai, " + nama);
-};
+console.log(hewan[0]); // Output: Kucing
+console.log(hewan[1]); // Output: Anjing
+console.log(hewan[2]); // Output: Kelinci
 
-sapa("Andi"); // Output: Hai, Andi
+let hewan = ["Kucing", "Anjing", "Kelinci"];
+hewan[1] = "Burung";
 
-const tambah = (a, b) => a + b;
+console.log(hewan); // Output: ["Kucing", "Burung", "Kelinci"]
 
-console.log(tambah(3, 5)); // Output: 8
+console.log(hewan.length); // Output: 3
 
-const buatUser = (nama, umur) => ({
-  nama: nama,
-  umur: umur
+buah.push("Mangga");
+
+console.log(buah); // ["Apel", "Pisang", "Jeruk", "Mangga"]
+
+buah.pop();
+
+console.log(buah); // ["Apel", "Pisang", "Jeruk"]
+
+buah.shift();
+
+console.log(buah); // ["Pisang", "Jeruk"]
+
+buah.unshift("Strawberry");
+
+console.log(buah); // ["Strawberry", "Pisang", "Jeruk"]
+
+buah.forEach(function(item, index) {
+  console.log(index, item);
 });
 
-console.log(buatUser("Rina", 20));
-/* Output:
-{ nama: 'Rina', umur: 20 }
-*/
+buah.forEach((item, index) => console.log(index, item));
+
+let angkaBaru = angka.map(function(x) {
+  return x * 2;
+});
+
+console.log(angkaBaru); // [2, 4, 6, 8, 10]
+
+let angkaGanjil = angka.filter(function(x) {
+  return x % 2 !== 0;
+});
+
+console.log(angkaGanjil); // [1, 3, 5]
+
+let siswa = ["Ali", "Budi", "Citra", "Dina"];
+
+// Menambahkan siswa baru
+siswa.push("Eka");
+
+// Menghapus siswa terakhir
+siswa.pop();
+
+// Menampilkan semua siswa
+siswa.forEach(function(nama) {
+  console.log(nama);
+});
