c554809261b975871fac3f43709a4e1c204a4c3a
StallPlayz <stallplayz123@gmail.com>
2025-09-19 07:55:15 +0700

    Update | Finishing 13th lesson

--- script.js
+++ script.js
@@ -1,87 +1,43 @@
-const angka = [1, 2, 3];
+const nama = "Dian";
 
-const satu = angka[0];
-const dua = angka[1];
-const tiga = angka[2];
+const kalimat = `Halo, nama saya ${nama}`;
 
-console.log(satu, dua, tiga); 
-// Output: 1 2 3
+console.log(kalimat);
+// Output: Halo, nama saya Dian
 
-const angka2 = [1, 2, 3];
+const nama1 = "Andi";
+const umur = 22;
 
-const [satu2, dua2, tiga2] = angka2;
+const perkenalan = `Halo, saya ${nama1}, umur saya ${umur} tahun.`;
 
-console.log(satu2, dua2, tiga2); 
-// Output: 1 2 3
+console.log(perkenalan);
 
-const angka3 = [1, 2, 3, 4];
+const a = 5;
+const b = 10;
 
-const [pertama, , ketiga] = angka3;
+console.log(`Hasil penjumlahan: ${a + b}`);
+// Output: Hasil penjumlahan: 15
 
-console.log(pertama); // 1
-console.log(ketiga);  // 3
+const bio = `Nama: Siti
+Umur: 20
+Asal: Yogyakarta`;
 
-const warna = ["merah", "biru", "hijau", "kuning"];
+console.log(bio);
 
-const [warna1, ...warnaLain] = warna;
+/* Output:
+Nama: Siti
+Umur: 20
+Asal: Yogyakarta */
 
-console.log(warna1);      // merah
-console.log(warnaLain);   // ["biru", "hijau", "kuning"]
+const title = "Belajar JS";
+const content = "Template Literals itu keren!";
 
-const user = {
-  nama: "Rudi",
-  umur: 30
-};
+const html = `
+  <div>
+    <h1>${title}</h1>
+    <p>${content}</p>
+  </div>
+`;
 
-const nama = user.nama;
-const umur = user.umur;
-
-console.log(nama, umur);
-
-const user1 = {
-  nama: "Rudi",
-  umur: 30
-};
-
-const { nama1, umur1 } = user1;
-
-console.log(nama1, umur1);
-
-const user2 = {
-  nama: "Rina",
-  umur: 28
-};
-
-const { nama: namaLengkap, umur: usia } = user2;
-
-console.log(namaLengkap); // Rina
-console.log(usia);        // 28
-
-const siswa = {
-  nama: "Ayu",
-  umur: 17,
-  kelas: "12 IPA"
-};
-
-const { nama2, ...infoLain } = siswa;
-
-console.log(nama2);      // Ayu
-console.log(infoLain);  // { umur: 17, kelas: '12 IPA' }
-
-const nama3 = "Budi";
-const umur2 = 20;
-
-const user3 = { nama3, umur2 };
-
-console.log(user3);
-// Output: { nama: 'Budi', umur: 20 }
-
-const angkasatu = 1;
-const angkadua = 2;
-const angkatiga = 3;
-
-const angka1 = [angkasatu, angkadua, angkatiga];
-
-console.log(angka1);
-// Output: [1, 2, 3]
+console.log(html);
 
